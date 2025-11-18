ec0af65c7dc28e50094dca88d34146bd35842c90
1WorthIte <158986580+1WorthIte@users.noreply.github.com>
2025-11-17 20:19:37 -0500

    Add files via upload

--- properties.py
+++ properties.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def compute_section_properties(sections, point_of_interest=None):
 
     layers = []
@@ -152,10 +150,4 @@ def compute_section_properties(sections, point_of_interest=None):
         'Q': Q_map,
     }
 
-def local_buckling(t, b, k, mu=0.2, E=4000):
-    beta = np.pi**2 * E / (12 * (1 - mu**2))
-    critical_stress = beta * k * (t/b)**2
-    return critical_stress
-
-print(compute_section_properties([(125, 300), (800, 100)], point_of_interest=286))
-print(local_buckling(1.27,18.75,0.425))
+print(compute_section_properties([(1.27, 68.73), (3.81, 2.5), (11.27, 1.27), (100, 1.27)], 58.72))
