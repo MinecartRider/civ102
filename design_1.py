32cf2a8060e25b22554758060c051dcccd53d6d3
Arthur Chen <arthur.chen04@gmail.com>
2025-11-18 17:31:59 -0500

    added archived design 1 calc

--- design_1.py
+++ design_1.py
@@ -1,4 +1,6 @@
 import properties as pr
+import cross_section as cs
+
 
 def shear_calculation(V, Q, I, b):
     return V * Q / I / b
@@ -14,3 +16,9 @@ print(f"Case 1 buckling {pr.local_buckling(2.54, 63.73, 4)}") #Centre of top
 print(f"Case 2 buckling {pr.local_buckling(2.54, 16.865, 0.425)}") #Flaps of top
 print(f"Case 3 buckling {pr.local_buckling(1.27, 22.846, 6)}") #stilts above centroidal axis
 print(f"Case 4 buckling {pr.shear_buckling(1.27, 97.46, 112)}")
+
+V = 242
+I = 3.37 * 10**6
+print(shear_calculation(V, 22605, I, 2* 1.27))
+print(shear_calculation(V, 16590, I, 12.54))
+
